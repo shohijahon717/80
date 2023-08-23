ec0a85ce64e5c71920e7d1d553e6afe17da7c5ee
Shohijahon <shohijahonyodgorov2141@gmail.com>
2023-08-23 22:04:51 +0500

    1

--- 1.py
+++ 1.py
@@ -1,1 +1,15 @@
-print('Salom')
+a = float(input('a = '))
+b = float(input("b = "))
+c = float(input('c = '))
+
+if a >= b:
+    if b >= c:
+        print(c)
+    if b < c:
+        print(b)
+elif b > a:
+    if a >= c:
+        print(c)
+    if a < c:
+        print(a)
+    
